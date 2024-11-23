ff67ad2de2c72d60f87fc512421d213e8e67a48c
Oksana <okssaloh@publicisgroupe.net>
2024-11-23 22:51:10 +0200

    feat: images

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { AppRouter } from './router';
 import { Notification } from './components/Notification';
 import { Provider } from 'react-redux';
 import store from './store';
-import './i18n';
-
 import 'react-datepicker/dist/react-datepicker.css';
 import 'semantic-ui-css/semantic.min.css';
+import './i18n';
 import './App.scss';
 
 function Situations() {
